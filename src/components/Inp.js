ed4dec8a7795f5a07c3dfa1496e90a4a8eeecda8
Yuvraj518 <124797389+Yuvraj518@users.noreply.github.com>
2023-12-23 13:03:59 +0530

    submitting code-Sat Dec 23 2023 13:03:55 GMT+0530 (India Standard Time)

---
+++ src/components/Inp.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const Inp=()=>{
+    const [para,setPara]=useState("");
+    function callAbc(event){
+        setPara(event.target.value);
+    }
+    return (<div>
+        <div><p>Enter your name:</p></div>
+        <div><input type="text"  onChange={callAbc} placeholder="Enter your name"></input></div>
+        <div><p> Hello {para}!</p></div>
+    </div>)
+}
+
+export default Inp;
